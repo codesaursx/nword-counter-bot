2ea4e342c47c63990202cda024186cd2ae4405bc
Angel Martinez <angel.martineztrc@gmail.com>
2023-10-03 14:48:18 -0500

    feat:  add more words

--- src/constants/target-words.constants.ts
+++ src/constants/target-words.constants.ts
@@ -1,5 +1,8 @@
 export const TARGET_WORDS = [
   String.fromCharCode(110, 105, 103, 103, 97),
+  String.fromCharCode(110, 105, 103, 103, 97, 115),
   String.fromCharCode(110, 101, 103, 114, 111),
-  String.fromCharCode(110, 105, 103, 103, 101, 114)
+  String.fromCharCode(110, 101, 103, 114, 111, 115),
+  String.fromCharCode(110, 105, 103, 103, 101, 114),
+  String.fromCharCode(110, 105, 103, 103, 101, 114, 115)
 ];
