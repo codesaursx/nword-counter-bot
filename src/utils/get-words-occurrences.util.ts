e844b0737256f4f3078008ddc313e0c749da922f
Angel Martinez <angel.martineztrc@gmail.com>
2023-10-03 14:55:52 -0500

    chore:  remove unnecesary console log

--- src/utils/get-words-occurrences.util.ts
+++ src/utils/get-words-occurrences.util.ts
@@ -1,6 +1,6 @@
 export const getWordsOccurrences = (s: string, words: string[]) => {
   const regex = new RegExp(s.split(' ').join('|'), 'g');
   const matches = (words.join(',').match(regex) ?? []).filter(v => v !== '');
-  console.log(matches);
+
   return matches.length;
 };
